94f68638cd2756117009dec1f88adcde2a337772
gunasekharravilla <gunasekharravilla156@gmail.com>
2021-06-23 21:20:09 +0530

    create backend

---
+++ Backend/data.js
@@ -0,0 +1,25 @@
+const data = {
+  events: [
+    {
+      _id: "1",
+      name: "Seminar on Mern Stack by Aravind",
+      image: "/images/p1.jpg",
+      rating: 4.5,
+      seatCount: 20,
+      numReviews: 11,
+      description:
+        "Attendees gain hands-on experience with the latest version of React and the tools for developing React applications.",
+    },
+    {
+      _id: "2",
+      name: "Seminar on React by Prashanth",
+      image: "/images/p2.jpg",
+      rating: 4,
+      seatCount: 0,
+      numReviews: 10,
+      description:
+        "Attendees gain hands-on experience with the latest version of React and the tools for developing React applications.",
+    },
+  ],
+};
+export default data;
