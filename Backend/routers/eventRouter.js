68707c8a3139bd6ada30b8b003cb46cae2873a28
gunasekharravilla <gunasekharravilla156@gmail.com>
2021-06-24 12:11:06 +0530

    event update api

--- Backend/routers/eventRouter.js
+++ Backend/routers/eventRouter.js
@@ -21,19 +21,38 @@ eventRouter.get(
     res.send({ createdEvents });
   })
 );
+
 eventRouter.post(
   "/",
   expressAsyncHandler(async (req, res) => {
     const event = new Event({
       title: "Event title " + Date.now(),
       image: "/images/p1.jpg",
-      seatCount: 0,
-      rating: 0,
-      numReviews: 0,
+      seatCount: 1,
+      rating: 1,
+      numReviews: 2,
       description: "sample description",
     });
     const createdEvent = await event.save();
     res.send({ message: "Event Created", event: createdEvent });
+  })
+);
+
+eventRouter.put(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const eventId = req.params.id;
+    const event = await Event.findById(eventId);
+    if (event) {
+      event.name = req.body.name;
+      event.image = req.body.image;
+      event.seatCount = req.body.countInStock;
+      event.description = req.body.description;
+      const updatedEvent = await event.save();
+      res.send({ message: "Event Updated", event: updatedEvent });
+    } else {
+      res.status(404).send({ message: "event Not Found" });
+    }
   })
 );
 
