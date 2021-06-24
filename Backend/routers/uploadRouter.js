8021bf56116b7ff58f8117f23a2b40124d135400
gunasekharravilla <gunasekharravilla156@gmail.com>
2021-06-24 22:24:55 +0530

    compress

--- Backend/routers/uploadRouter.js
+++ Backend/routers/uploadRouter.js
@@ -1,21 +1,39 @@
 import multer from "multer";
 import express from "express";
+import sharp from "sharp";
 
 const uploadRouter = express.Router();
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`);
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+uploadRouter.use(express.static("./uploads"));
 
-uploadRouter.post("/", upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename(req, file, cb) {
+//     cb(null, `${Date.now()}.jpg`);
+//   },
+// });
+
+uploadRouter.post("/", upload.single("image"), async (req, res) => {
+  fs.access("./uploads", (error) => {
+    if (error) {
+      fs.mkdirSync("./uploads");
+    }
+  });
+  const { buffer, originalname } = req.file;
+  const timestamp = new Date().toISOString();
+  const ref = `${timestamp}-${originalname}.jpg`;
+  await sharp(buffer)
+    .jpg({ quality: 20 })
+    .toFile("./uploads/" + ref);
+  const link = `http://localhost:3000/${ref}`;
+  return res.json({ link });
 });
+
+// uploadRouter.post("/", upload.single("image"), (req, res) => {
+//   res.send(`/${req.file.path}`);
+// });
 
 export default uploadRouter;
